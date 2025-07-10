20a670936b23a8b9fc4d2cb26337b99a83f303f4
CreoCot <120632527+CreoCot@users.noreply.github.com>
2025-07-10 11:25:57 +0300

    Text has more flexibility

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,22 +1,57 @@
 import styles from "./Text.module.css";
 import React from "react";
 
+type TextElement =
+  | "p"
+  | "span"
+  | "div"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "li";
+
+type TextAlign = "left" | "center" | "right" | "justify";
+type TextTransform = "none" | "capitalize" | "uppercase" | "lowercase";
+type DisplayType = "inline" | "block" | "inline-block";
+
+type FontWeight = "regular" | "bold";
+type FontSize = "small" | "medium" | "large" | "huge";
+type ColorVariant = "default" | "primary" | "secondary" | "button" | "hover";
+
 interface TextProps {
-  textContent: React.ReactNode;
-  fontWeight?: string | number;
-  fontSize?: "small" | "medium" | "large" | "huge";
-  colorVariant?: "default" | "primary" | "secondary" | "button" | "hover";
+  as?: TextElement;
+  children: React.ReactNode;
+  fontWeight?: FontWeight;
+  fontSize?: FontSize;
+  colorVariant?: ColorVariant;
   interactable?: boolean;
+  textAlign?: TextAlign;
+  textTransform?: TextTransform;
+  display?: DisplayType;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  style?: React.CSSProperties;
 }
 
 function Text(props: Readonly<TextProps>) {
   const {
-    textContent,
+    children,
     fontSize = "medium",
-    fontWeight = "normal",
+    fontWeight = "regular",
     colorVariant = "default",
     interactable = false,
+    onClick,
+    textTransform = "none", // Default textTransform
+    display, // Destructure display
+    className, // Additional class names
+    style, // Additional inline styles
+    as = "p",
   } = props;
+
+  const Component = as;
 
   const fontSizeClass = {
     small: styles.fontSizeSmall,
@@ -33,20 +68,24 @@ function Text(props: Readonly<TextProps>) {
     hover: styles.colorHover,
   }[colorVariant];
 
+  const classes = [styles.text, className, colorClass, fontSizeClass]
+    .filter(Boolean)
+    .join(" ");
+
   const inlineStyles: React.CSSProperties = {
-    fontWeight: fontWeight,
+    fontWeight: fontWeight === "bold" ? "bold" : "regular",
     userSelect: interactable ? "auto" : "none",
     WebkitUserSelect: interactable ? "auto" : "none",
     MozUserSelect: interactable ? "auto" : "none",
+    textTransform: textTransform,
+    display: display,
+    ...style,
   };
 
   return (
-    <p
-      className={`${styles.text} ${colorClass} ${fontSizeClass}`}
-      style={inlineStyles}
-    >
-      {textContent}
-    </p>
+    <Component className={classes} style={inlineStyles} onClick={onClick}>
+      {children}
+    </Component>
   );
 }
 
